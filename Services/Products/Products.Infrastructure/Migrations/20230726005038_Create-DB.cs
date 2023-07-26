530b07efdb0b3cce195241e5d6ef741780e0bd76
Leonardo <leo.bruni130@gmail.com>
2023-07-25 22:04:31 -0300

    switched project to migrations

---
+++ Services/Products/Products.Infrastructure/Migrations/20230726005038_Create-DB.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Products.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    TenantId = table.Column<int>(type: "int", nullable: false),
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(24)", maxLength: 24, nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(24)", maxLength: 24, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(24)", maxLength: 24, nullable: true),
+                    ModifiedBy = table.Column<int>(type: "int", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Active = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TenantId_PersonId", x => new { x.TenantId, x.PersonId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    TenantId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(5,2)", nullable: true),
+                    ProductTypeId = table.Column<int>(type: "int", nullable: true),
+                    ModifiedBy = table.Column<int>(type: "int", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Active = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductId_TenantId", x => new { x.ProductId, x.TenantId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductType",
+                columns: table => new
+                {
+                    TenantId = table.Column<int>(type: "int", nullable: false),
+                    ProductTypeId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: true),
+                    ModifiedBy = table.Column<int>(type: "int", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Active = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTypeId_TenantId", x => new { x.ProductTypeId, x.TenantId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tenant",
+                columns: table => new
+                {
+                    TenantId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    ModifiedBy = table.Column<int>(type: "int", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Active = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TenantId", x => x.TenantId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "ProductType");
+
+            migrationBuilder.DropTable(
+                name: "Tenant");
+        }
+    }
+}
---
+++ Services/Products/Products.Infrastructure/obj/Debug/net6.0/kl0cxss5.sql
@@ -0,0 +1,69 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Person] (
+    [TenantId] int NOT NULL,
+    [PersonId] int NOT NULL,
+    [Name] nvarchar(24) NULL,
+    [LastName] nvarchar(24) NULL,
+    [Email] nvarchar(24) NULL,
+    [ModifiedBy] int NULL,
+    [DateCreated] datetime NULL,
+    [Active] bit NULL,
+    CONSTRAINT [PK_TenantId_PersonId] PRIMARY KEY ([TenantId], [PersonId])
+);
+GO
+
+CREATE TABLE [Product] (
+    [TenantId] int NOT NULL,
+    [ProductId] int NOT NULL,
+    [Name] nvarchar(60) NULL,
+    [Description] nvarchar(120) NULL,
+    [Price] decimal(5,2) NULL,
+    [ProductTypeId] int NULL,
+    [ModifiedBy] int NULL,
+    [DateCreated] datetime NULL,
+    [Active] bit NULL,
+    CONSTRAINT [PK_ProductId_TenantId] PRIMARY KEY ([ProductId], [TenantId])
+);
+GO
+
+CREATE TABLE [ProductType] (
+    [TenantId] int NOT NULL,
+    [ProductTypeId] int NOT NULL,
+    [Name] nvarchar(60) NULL,
+    [Description] nvarchar(120) NULL,
+    [ModifiedBy] int NULL,
+    [DateCreated] datetime NULL,
+    [Active] bit NULL,
+    CONSTRAINT [PK_ProductTypeId_TenantId] PRIMARY KEY ([ProductTypeId], [TenantId])
+);
+GO
+
+CREATE TABLE [Tenant] (
+    [TenantId] int NOT NULL,
+    [Name] nvarchar(30) NULL,
+    [ModifiedBy] int NULL,
+    [DateCreated] datetime NULL,
+    [Active] bit NULL,
+    CONSTRAINT [PK_TenantId] PRIMARY KEY ([TenantId])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230726004312_Night-Prototype', N'7.0.2');
+GO
+
+COMMIT;
+GO
+
